e500b74e0098b9f5e5ced09d12007e90ddaaa827
fumi-toyama <fumiya.toyama@woven-planet.global>
2023-01-26 17:41:53 +0900

    fix_drinkjs

--- vending-machine.js
+++ vending-machine.js
@@ -1,24 +1,47 @@
+const  drink = {
+    greentea: [10, 120],
+    cola: [10, 150],
+    orange: [10, 300],
+    cclemon: [10, 150],
+    beer: [10, 50]
+}
 
+let total_u_money = 0;
+// お金入れる
+let u_money = 200;
 
-/*
-:エビフライ:Vending Machine
-目的
-We will make a Vending Machine Program!
-開発条件
-(好きなデータ構造とやり方でいいですよ！)
-難易度： 村人
-Vending Machineには以下の機能を作成せよ！
-drinkの在庫
-Userがお金を入れる
-現在の金額の表示、もしくは確認
-Drinkを選ぶ (在庫から-1)
-お金が不足しているかDrinkの在庫がない場合、エラーメッセージが出る
-Drinkの一覧を出す
-難易度： 忍者
-上記の機能全て
-全てのdrinkの在庫を元に戻す機能（補充）
-補充した後、入れたDrinkの名前と入れた数を表示
-難易度： ナイトメアー
-上記の機能全て
-お釣りを出す時、正しいコインで出す. 例：{100:1, 50:1, 1:4}
-*/
+function preview(total_u_money, u_money) {
+    total_u_money += u_money
+    console.log(total_u_money)
+    return total_u_money
+};
+
+// preview(total_u_money, 200);
+
+function select_drink(u_select){
+    switch(u_select){
+        case(drink.greentea):
+            drink.greentea[0] -= 1;
+            check(drink.greentea[0]);
+            console.log(drink.greentea[0]);
+            break;
+        case(drink.beer):
+            drink.beer[0] -= 1;
+            check(drink.beer[0]);
+            console.log(drink.beer[0]);
+            break;
+        default:
+            console.log(drink);
+    }
+}
+
+// select_drink(drink.cola);
+
+function check(num){
+    if(num <= 0){
+        console.log("sold out...残念！次がんばろう！！")
+    } else {
+        console.log("I got it! Your lucky!!")
+    }
+}
+
